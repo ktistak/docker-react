baf508d6673b6ac674bc9c553b164e1b35fe625e
Georgios Ktistakis <Georgios.Ktistakis@ortec.com>
2023-05-05 16:58:06 +0300

    a message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi from ktist@k!
+          Hi from ktist@k there!
         </p>
         <a
           className="App-link"
